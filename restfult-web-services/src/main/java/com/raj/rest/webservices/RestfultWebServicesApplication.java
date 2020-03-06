995b973c728727fe19a58713782fbcc6d2066396
rajkhare <rajkhare@192.168.31.81>
2020-03-07 05:25:03 +0530

    first commit

---
+++ restfult-web-services/src/main/java/com/raj/rest/webservices/RestfultWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.raj.rest.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfultWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfultWebServicesApplication.class, args);
+	}
+
+}
---
+++ restfult-web-services/src/test/java/com/raj/rest/webservices/RestfultWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.rest.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfultWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
